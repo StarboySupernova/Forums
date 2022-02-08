0331a33597cbf2b4f1de30128086224cb6f7d790
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2022-02-08 00:20:06 +0000

    Initial Commit

---
+++ Forums/ForumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForumsApp.swift
+//  Forums
+//
+//  Created by user213454 on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ForumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
